8e01e0dbb03311c3ea126320ca999f6c12b2a804
archieross <59793921+archieross@users.noreply.github.com>
2024-12-30 15:17:44 +0000

    Delete messageClass.py

--- messageClass.py
+++
@@ -1,8 +0,0 @@
-class Communication:
-
-    def __init__(self, messageType, message, sender=None, recipient=None):
-
-        self.messageType = messageType
-        self.message = message
-        self.sender = sender
-        self.recipient = recipient
